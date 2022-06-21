eb60a4dbfb9527cc05d5450083dbef914b2a888c
WWW-max <464711201@qq.com>
2022-06-21 23:54:20 +0800

    feat: 增加登录组件

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import Login from '../Login/Login';
 
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/login" element={<Login/>}/>
+              </Routes>
+          </BrowserRouter>
+      </div>
     )
   }
 }

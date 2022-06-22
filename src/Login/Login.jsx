a1d8ad373500c200778fbb0659d514470bf6359e
WWW-max <464711201@qq.com>
2022-06-22 23:58:35 +0800

    feat: 导入antd组件库

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,9 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Input, Button } from 'antd';
 
 export default class Login extends Component {
   render() {
     return (
-      <div>Login</div>
+      <div className='login'>
+        账号：<Input/><br/><br/><br/>
+        密码：<Input/><br/>
+        <Button type="primary">登录</Button>&nbsp;  
+        <Button type="primary">重置</Button>  
+      </div>
     )
   }
 }

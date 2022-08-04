81bdaeb26a84abc9b9bf458c9a89d4cac385421a
WWW-max <464711201@qq.com>
2022-08-04 23:54:55 +0800

    feat: 登录组件改为函数组件

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,62 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Input, Button } from 'antd';
 import 'antd/dist/antd.css';
 import './Login.css';
 import axios from 'axios';
 
-export default class Login extends Component {
-  state = {
-    username:'', // 用户名
-    password:'', // 密码
-  }
-  signIn = () => {
-    console.log('登录');
-    const {username, password} = this.state;
-    let param = {
-        username,
-        password,
-    }
-    axios.post('/api/login ',param).then(function(response){
-        console.log(response); 
-    }).catch(function(error){
-        console.log('用户名或密码错误');
-    })
-  }
-  reset = () => {
-    console.log('重置');
-    this.setState({
-        username:'',
-        password:'',
-    })
-  }
-  usernameChange = (e) => {
-    console.log(e.target.value);
-     this.setState({
-        username: e.target.value
-     })
-  }
-  passwordChange = (e) => {
-    console.log(e.target.value);
-    this.setState({
-        password: e.target.value
-    })
-  }
-  render() {
-    const {username, password} = this.state;
-    return (
-     <div className='container'>
-       <div className='login'>
-        <div className='header'>《数据结构》在线考试系统</div>
-        <div className='main'>
-            账号：<Input style={{width:'250px'}} type='text' value={username} onChange={this.usernameChange}/><br/><br/><br/>
-            密码：<Input style={{width:'250px'}}type='password' value={password} onChange={this.passwordChange}/><br/>
-        </div>
-        <div className='footer'>
-            <Button type="primary" onClick={this.signIn}>登录</Button>&nbsp;&nbsp;&nbsp;  
-            <Button type="primary" onClick={this.reset}>重置</Button>  
-        </div>
-      </div>
+export default function Login() {
+  const [userName,setUserName] = useState('');
+  const [passWord,setPassWord] = useState('');
+  return (
+    <div className='container'>
+    <div className='login'>
+     <div className='header'>《数据结构》在线考试系统</div>
+     <div className='main'>
+         账号：<Input style={{width:'250px'}} type='text' value={username} onChange={this.usernameChange}/><br/><br/><br/>
+         密码：<Input style={{width:'250px'}}type='password' value={password} onChange={this.passwordChange}/><br/>
      </div>
-    )
-  }
+     <div className='footer'>
+         <Button type="primary" onClick={signIn()}>登录</Button>&nbsp;&nbsp;&nbsp;  
+         <Button type="primary" onClick={reset()}>重置</Button>  
+     </div>
+   </div>
+  </div>
+  )
 }
